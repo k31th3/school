5372d3dc8ca7409f043110991fe92923a8c61859
Keith3 <keith3deyta@gmail.com>
2026-04-15 16:07:33 +0800

    Data.ts
    - fix dataType of title

--- src/mockData/data.ts
+++ src/mockData/data.ts
@@ -34,44 +34,46 @@ export const NavbarMenu: NavbarItem[] = [
 		link: "#"
 	},
 	{
-		id: 4,
-		dropdown: {
-			title: "crt online",
-			option: [
-				{
-					id: 1,
-					title: "crt careers connect",
-					link: "https://crtcareers.seemeconnect.com/"
-				},
-				{
-					id: 2,
-					title: "crt lms student",
-					link: "https://crt.edu.ph/lms/student/#/login"
-				}
-			]
-		}
+	  	id: 4,
+	  	title: "CRT Online",
+	  	dropdown: {
+	    	title: "crt online",
+	    	option: [
+		      	{
+		        	id: 1,
+		        	title: "crt careers connect",
+		        	link: "https://crtcareers.seemeconnect.com/"
+		      	},
+		      	{
+		        	id: 2,
+		        	title: "crt lms student",
+		        	link: "https://crt.edu.ph/lms/student/#/login"
+		      	}
+	    	]
+	  	}
 	},
 	{
-		id: 5,
-		dropdown: {
-			title: "admission",
-			option: [
-				{
-					id: 1,
-					title: "college",
-					link: "https://docs.google.com/forms/d/e/1FAIpQLSfZOilIA20jzKHsMmVlH8_EBZGBCx5oVHz07QGxs6cWEY5WbA/viewform"
-				},
-				{
-					id: 2,
-					title: "junior high school",
-					link: "https://docs.google.com/forms/d/e/1FAIpQLScgoUFWRhF-1Y4Bpofbg3ysYugTidlQHkFqJgNCMv36L9ZuTg/viewform"
-				},
-				{
-					id: 3,
-					title: "senior high school",
-					link: "https://docs.google.com/forms/d/e/1FAIpQLSf1T9mFINOuS-N7UaxM8RwWz0_aGjAS4j0ieLwYBhw_euFq9Q/viewform"
-				}
-			]
+	  id: 5,
+		title: "Admissions",
+		  	dropdown: {
+		    	title: "admission",
+		    	option: [
+			    {
+			        id: 1,
+			        title: "college",
+			        link: "https://docs.google.com/forms/d/e/1FAIpQLSfZOilIA20jzKHsMmVlH8_EBZGBCx5oVHz07QGxs6cWEY5WbA/viewform"
+			    },
+			    {
+			        id: 2,
+			        title: "junior high school",
+			        link: "https://docs.google.com/forms/d/e/1FAIpQLScgoUFWRhF-1Y4Bpofbg3ysYugTidlQHkFqJgNCMv36L9ZuTg/viewform"
+			    },
+			    {
+			        id: 3,
+			        title: "senior high school",
+			        link: "https://docs.google.com/forms/d/e/1FAIpQLSf1T9mFINOuS-N7UaxM8RwWz0_aGjAS4j0ieLwYBhw_euFq9Q/viewform"
+			    }
+		    ]
 		}
 	},
 	{
