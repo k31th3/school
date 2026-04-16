499738ecf2d61672c39c06dca42d45cd90bacb63
Keith3 <keith3deyta@gmail.com>
2026-04-16 13:48:07 +0800

    Animation:Delay
    - add type number

--- src/utility/HomeAnimation.tsx
+++ src/utility/HomeAnimation.tsx
@@ -1,18 +1,15 @@
 
-export const SlideRight = (delay) => {
-	return {
-		hidden: {
-			opacity: 0,
-			x: -100,
+export const SlideRight = (delay: number) => ({
+	hidden: {
+		opacity: 0,
+		x: -100,
+	},
+	visible: {
+		opacity: 1,
+		x: 0,
+		transition: {
+			duration: 1,
+			delay,
 		},
-		visible: {
-			opacity: 1,
-			x: 0,
-			transition: {
-				duration: 1,
-				delay: delay
-			}
-		},
-		
-	}
-}
+	},
+});
